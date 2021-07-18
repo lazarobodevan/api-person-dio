6cee63976282e30e68557db85cb12a27c935e0df
Lázaro Bodevan <lazaro.maia@ufv.br>
2021-07-18 03:23:07 -0300

    Get person by id

--- src/main/java/one/digitalinnovation/personapi/service/PersonService.java
+++ src/main/java/one/digitalinnovation/personapi/service/PersonService.java
@@ -3,15 +3,13 @@ package one.digitalinnovation.personapi.service;
 import one.digitalinnovation.personapi.dto.MessageResponseDTO;
 import one.digitalinnovation.personapi.dto.request.PersonDTO;
 import one.digitalinnovation.personapi.entity.Person;
+import one.digitalinnovation.personapi.exception.PersonNotFoundException;
 import one.digitalinnovation.personapi.mapper.PersonMapper;
 import one.digitalinnovation.personapi.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-import java.util.ListIterator;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
@@ -40,5 +38,10 @@ public class PersonService {
         List<Person> allPeople = personRepository.findAll();
         List<PersonDTO> allPeopleDTO = null;
         return allPeople.stream().map(personMapper::toDTO).collect(Collectors.toList());
+    }
+
+    public PersonDTO findById(Long id) throws PersonNotFoundException {
+       Person person = personRepository.findById(id).orElseThrow(()-> new PersonNotFoundException(id));
+        return personMapper.toDTO(person);
     }
 }
